70e2c5feeff2bb79afe9123149d597313eff8dc5
zaher <nzaher1981@gmail.com>
2024-12-15 18:16:57 +0100

    Create Bonjour.aspx.cs

---
+++ Bonjour.aspx.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Web.UI;
+
+namespace TonNamespace
+{
+    public partial class Bonjour : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Aucun code nécessaire ici pour cette simple page
+        }
+    }
+}
